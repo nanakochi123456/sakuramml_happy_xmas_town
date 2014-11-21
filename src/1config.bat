70de936249cf85f8acf3ebee9dc8230f58cbad81
nanoko <yy845jp@gmail.com>
2014-11-21 11:39:20 +0900

    first step2

---
+++ src/1config.bat
@@ -0,0 +1,11 @@
+rem Å´à»â∫ÇÇ©Ç¶ÇƒÇ›ÇÈ
+SET TIMIDITYHOME=c:\happy_xmas_town
+SET INPUT=c:\happy_xmas_town
+SET OUTPUT=%INPUT%
+rem Å™Ç±Ç±Ç‹Ç≈
+
+SET TIMIDITYBIN=%TIMIDITYHOME%\Timidity++
+SET TIMIDITYPAT=%TIMIDITYHOME%\Patches
+SET TIMIDITYCFG=%TIMIDITYHOME%\Patches\cfgs
+
+SET TIMIDITYRCFGPATH=../patches/cfgs
---
+++ src/project.bat
@@ -0,0 +1,7 @@
+SET TITLE=ハッピークリスマスタウン
+SET CONFIG=happyxmastown.cfg
+SET PLAYOPTION=--output-stereo --sampling-freq=48000 -EFchorus=n,10 -EFreverb=d,120 --output-16bit
+rem SET OPTION=--output-stereo --sampling-freq=48000 -EFchorus=n,10 -EFreverb=d,120 --output-16bit
+SET OPTION=--output-stereo --sampling-freq=48000 -EFchorus=n,10 -EFreverb=d,120 --output-16bit
+SET MIDI1=happyxmastown.mid
+SET MIDI1PLAY=1
